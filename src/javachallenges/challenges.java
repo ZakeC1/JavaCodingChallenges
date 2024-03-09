3e663dd42cb3b29f89920eaa3ec517ff6892ca4b
Zi Chen <532764059@qq.com>
2024-03-09 16:01:04 +0000

    Initial commit

---
+++ src/javachallenges/challenges.java
@@ -0,0 +1,17 @@
+package javachallenges;
+
+public class challenges {
+	//Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+	public static int sumDouble(int a, int b) {
+		if(a == b) {
+			return (4*a);
+		} else {
+			return (a+b);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}

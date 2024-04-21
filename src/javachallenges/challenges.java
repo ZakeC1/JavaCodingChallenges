a086c3443fb156b8b2fc721ee762498a2a21d04d
Zi Chen <532764059@qq.com>
2024-04-21 13:23:59 +0100

    Improved Method For Primer Tester

--- src/javachallenges/challenges.java
+++ src/javachallenges/challenges.java
@@ -157,7 +157,7 @@ public class challenges {
 	    	return false;
 	    }
 	    
-	    for (int i = 2; i <= num / 2; i++) {
+	    for (int i = 2; i <= Math.sqrt(num); i++) {
 	    	if (num % i == 0) {
 	    		return false;   
 	    	}

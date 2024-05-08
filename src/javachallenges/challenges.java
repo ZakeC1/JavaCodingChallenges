fb913ae1c2b4c11dfe298878786d46b517e362e4
Zi Chen <532764059@qq.com>
2024-05-08 14:45:58 +0100

    Mirrored Star Pyramid Solution

--- src/javachallenges/challenges.java
+++ src/javachallenges/challenges.java
@@ -168,7 +168,16 @@ public class challenges {
 	
 	//Define a function that outputs a mirrored pyramid of stars
 	public static void stars() {
-		
+		int size = 10;
+		for (int i = 0; i< size; i++) {
+			for (int j = size; j  >  i; j--) {
+                System.out.print(" ");
+            }
+			for (int k = 0; k  <= 2*i; k++) {
+                System.out.print("*");
+            }
+			System.out.println();
+		}
 	}
 	
 	public static void main(String[] args) {
